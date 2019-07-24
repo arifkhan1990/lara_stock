ce5f905da3948b0c4c8e9e7e5d05cab72ce6db98
ripan <ripanmia.pau1122@gmail.com>
2019-07-24 19:40:53 +0600

    login registration forgot password complete

--- routes/web.php
+++ routes/web.php
@@ -16,28 +16,14 @@ Route::get('/', function () {
 });
 
 
-
-Route::resource('/admin', 'AdminController');
-Route::resource('/admin/user', 'UserController');
-
-
-Route::get('/home', function () {
-    return view('home');
-});
-
-Route::get('/signin', function () {
-    return view('login');
-});
-
-Route::get('/logout', function () {
-    Auth::logout();
-    return Redirect::to('');
-})->middleware("auth");
-
-Route::get('/register', function () {
-    return view('register.register');
-});
-
-
-Route::post('/register_action','RegisterController@store');
-Route::post('/login_check','LoginController@login');
+Route::get('/admin_register','Admin\RegistationController@index')->name('register');
+Route::post('/admin_register/store','Admin\RegistationController@store')->name('register.store');
+Route::get('/confirm_email/{id}','Admin\RegistationController@confirm_email')->name('confirm_email');
+Route::get('/admin_login','Admin\LoginController@index')->name('admin_login');
+Route::get('/admin_login/check','Admin\LoginController@check')->name('admin_login.check');
+Route::get('/dashboard','Admin\DashboardController@dashboard')->name('dashboard')->middleware('admin');
+Route::get('/forgot_password','Admin\LoginController@forgot')->name('forgot');
+Route::post('/forgot_password/check','Admin\LoginController@forgot_check')->name('forgot_check');
+Route::get('/forgot_password/update/{id}','Admin\LoginController@forgot_update')->name('forgot_update');
+Route::post('/forgot/update/{id}','Admin\LoginController@forgot_new')->name('forgot_new');
+Route::get('/logout','Admin\LoginController@logout')->name('logout');
